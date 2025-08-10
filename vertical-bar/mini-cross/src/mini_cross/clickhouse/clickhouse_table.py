4dcf49665f0bc6eccf72fe19b448320da6c3fa2a
hoang <hoang.np193@gmail.com>
2025-08-10 17:27:46 +0700

    add description

--- vertical-bar/mini-cross/src/mini_cross/clickhouse/clickhouse_table.py
+++ vertical-bar/mini-cross/src/mini_cross/clickhouse/clickhouse_table.py
@@ -1,12 +1,17 @@
 from typing import List, Dict, Any
 
 from mini_cross.cross_table import Table
-
 """
 A separate primary.idx file has the value of the primary key for each N-th row (index_granularity).
 Also, for each column, we have column.mrk files with "marks", which are offsets (byte position) 
-    to each N-th row in the data file
+    to each N-th row in the data file. this one to deal compression requirement.
+    
+ClickHouse is not suitable for a high load of simple point queries, 
+because the entire range with index_granularity rows must be read for each key, 
+and the entire compressed block must be decompressed for each column
 """
+
+
 class ClickhouseTable(Table):
 
     @classmethod
